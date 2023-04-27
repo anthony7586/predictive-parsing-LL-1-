adab6946c0e8a9282637c210c70f1588b74799d7
ANTHONY RUIZ <46951916+anthony7586@users.noreply.github.com>
2023-04-27 13:49:18 -0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -20,14 +20,15 @@ int main(){
     string og_input = ""; //will hold original user input
 
   
-    char curr_stack_char;//char being compared
-    char curr_input_char;//char being compared
+    char last_stack_char;//char being compared
+    char first_input_char;//char being compared
 
     //start with user input strings
     cout << "input the string.\n";
     cin >> og_input; 
     input = og_input;// make a copy of string so you can save og for later 
-    
+
+
 
 
             /*      these will be test strings 
@@ -39,38 +40,40 @@ int main(){
             
             */  
                     
-            //the next few comments will be assuming the input is (a +a )*a$        
+                   
 
-    //get the length of strings to later use as index  
-    int og_input_length = og_input.length(); 
+    
    
     //load the stack columb with a $ and the first non terminal, push input, and push empty for output first row  
     stack_coll.push("$E");      input_coll.push(og_input);          output_coll.push(" ");
     
     //now that we have string we will use given Predictive parsing table to compare.
         //setting current variable to later be compared 
+while(1){      
+       
+        //assign the last_stack_char and first_input_char so that we can evaluate
         curr_stack_string = stack_coll.back();
-        curr_stack_char = curr_stack_string.back();   
-        curr_input_char = input[0];
+        last_stack_char = curr_stack_string.back();   
+        first_input_char = input[0];
 
                 //if there is a space in the first char of input then delete it 
                     if(input[0] == ' ' ){
                     input = input.erase(0,1); //erase the first char in the string 
-                    curr_input_char = input[0]; 
+                    first_input_char = input[0]; 
                     }
 
 
 
-     if (curr_stack_char != curr_input_char) //if the last stck char and first input are NOT equivelent execute 
+     if (last_stack_char != first_input_char) //if the last stck char and first input are NOT equivelent execute 
      {
         //look for production rule
-        curr_pro_rule = production_rule(curr_stack_char, curr_input_char);
+        curr_pro_rule = production_rule(last_stack_char, first_input_char);
         //push current production into output queue
         output_coll.push(curr_pro_rule);
 
 
         //replace last char in curr_stack_string with the production rule (in reverse)
-        rev_prod = curr_pro_rule; //curr_pro_rule is reversed in next line and stored in rev_prod
+         rev_prod = curr_pro_rule; //curr_pro_rule is reversed in next line and stored in rev_prod
          reverse(rev_prod.begin(), rev_prod.end());
                 
                 //now we replace the char in the curr_stack_string with the new production rule as long as its not epsilon 
@@ -81,38 +84,45 @@ int main(){
                         curr_stack_string.append(rev_prod); //appends the reveresed production 
                         //push to the stack_coll queue 
                         stack_coll.push(curr_stack_string);
+                        input_coll.push(input);}
+                     
+                     if (rev_prod == "ɛ"){ // if we have ɛ then we delete last_stack_char in stack and put the string back on 
+                        curr_stack_string.erase(curr_stack_string.end());
+                        stack_coll.push(curr_stack_string);
                         input_coll.push(input);
-                    }
+                     }
             
 
-        //end  of if, continue chacking...
-     }
+        
+     }//end  of if, continue chacking...
 
-        //reset the current chars so that we can re-evaluate 
+
+
+
+        //reassign the last_stack_char and first_input_char so that we can re-evaluate
         curr_stack_string = stack_coll.back();
-        curr_stack_char = curr_stack_string.back();
-        //curr_input_string = input_coll.back();
-        curr_input_char = input[0];
+        last_stack_char = curr_stack_string.back();
+        first_input_char = input[0];
 
      
-     if ( (curr_stack_char == curr_input_char) ) //if the last stck char and first input are equivelent execute 
+
+
+
+
+     if ( (last_stack_char == first_input_char) ) //if the last stck char and first input are equivelent execute 
      {
-            //checking for end 
-            if ((curr_stack_char == '$') && (curr_input_char == '$'))
-            {
-                
-                //display all queues, stack_coll, input_coll, output_coll------------------------------------------------------
-                output_coll.push("string is accepted/valid\n");
-                //can be done iterativley using for loops ------------------------------------------------------------------------
-                
-                return 0;
+            
+            if ((last_stack_char == '$') && (first_input_char == '$'))//checking for end/ accepted 
+            {                
+                display_all(stack_coll,input_coll,output_coll);//display all queues, stack_coll, input_coll, output_coll
+                cout << "string is accepted/valid\n";
+                return 0; //ends the loop 
             }
 
 
         //delete last char from curr_stack_string, then push it into the stack queue 
         curr_stack_string.pop_back();
         stack_coll.push(curr_stack_string);
-
         //delete first char from input, then push it onto the input queue  
         input.erase(0,1);
         input_coll.push(input);
@@ -123,19 +133,48 @@ int main(){
      }
 
     
-      //reset the current chars so that we can re-evaluate, this is done at top but we need to loop back --------------------------
-      //loop until the curr_stack_char is $ and also equal to a curr_input_char of $
+
+
 
         
 
-        //how do we check if it is not accepted? -------------------------------------------------------------------
-        //also display all queues, stack_coll, input_coll, output_coll------------------------------------------------------
+        //need to still check if not accepted. -------------------------------------------------------------------
+        
+        //if not accepted display contents of queue, end the loop-----------------------------------------
+                //display_all(stack_coll,input_coll,output_coll);
+                //cout << "string not accepted.\n ";
     
 
 
 
+
+//loop back to the top, untul not accepted or accepted 
+ //reassign the last_stack_char and first_input_char so that we can re-evaluate, this is done at top of the loop
+} //end of while loop
+
 return 0;
-};
+};//end of main 
+
+
+
+
+//this function taks in the queues and displays them, executes when you know if string is accepted or not accepted and 
+//we want to pisplay the data 
+void display_all(queue<string> stack_coll, queue<string> input_coll, queue<string> output_coll){
+
+    queue<string> s = stack_coll; //make a copy, so we can display each queue 
+    queue<string> i = input_coll;
+    queue<string> o = output_coll;
+
+    while (!s.empty()) ///all queues shoudl be equal in size so you should just have to check length of 1
+    {
+        cout << s.front() << "   |   " << i.front() << "   |   " << o.front() << "   |   " << endl;  
+        
+        s.pop(); i.pop(); o.pop(); //clear the first items in the queue, then repreat.
+
+    }
+
+}
 
 
 
